783a048cf302716f754dde61c5d79e38c9fbf639
U-RODOLFO\RudeB <espanharodofera@gmail.com>
2024-05-19 23:24:13 -0300

    Testando Movimentação no Update()

--- MyGame.py
+++ MyGame.py
@@ -1,10 +1,14 @@
 from game import GameEngine
 import engine
 
-# definir cores
+# Definir cores
 red   = (255, 0, 0)
 green = (0, 255, 0)
 blue  = (0, 0, 255)
+
+# Atributos do jogador
+x = 30
+y = 30
 
 class MyGame(GameEngine):
     def __init__(self, width, height):
@@ -12,20 +16,22 @@ class MyGame(GameEngine):
         # Inicialize os elementos do jogo aqui
 	
     def update(self):
-        # Atualize a lógica do jogo específico aqui
+        global x  # Informe ao Python que estamos nos referindo à variável global x
+        global y
         super().update()
-
+        x += 1  # Atualize a posição do jogador
+        y += 1
+    
     def render(self):
         super().render()
-        # Renderize os elementos do jogo específico aqui
-        self.screen.fill(red)  # Usando a cor RGB para azul
-        engine.pygame.draw.rect(self.screen, (green), (40, 50, 70, 80))
+        # Renderize os elementos do jogo aqui
+        self.screen.fill(red)  # Usando a cor vermelha
+        engine.pygame.draw.rect(self.screen, green, (x, y, 70, 80))  # Corrigido a posição do retângulo
         engine.pygame.display.flip()  # Atualiza a tela
 	
 def main():
     game = MyGame(300, 480)
     game.run()
 
-
-
-
+if __name__ == "__main__":
+    main()
